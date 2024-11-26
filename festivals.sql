166b550bf2d3f9c7570b9aff60b0485cbb5b45b1
RoulioDev <jules.webdevplus@gmail.com>
2024-11-26 23:50:32 +0100

    Update festivals.sql

--- festivals.sql
+++ festivals.sql
@@ -23,7 +23,8 @@ CREATE TABLE festival_concerts (
     oeuvre_compositeur VARCHAR(255),         
     oeuvre_date_creation DATE,              
     oeuvre_duree INT   
-    --commentaire                       
+    --commentaire    
+    --encore un commentaire
 );
 
 
